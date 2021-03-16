9b76def480131efce1a645b84b888428831f2bd8
DvynilHub <belonogov220@gmail.com>
2021-03-16 21:52:40 +0800

    111

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,7 @@ public class Main {
         while (!pass.isEquals()){
             pass.setPassword(in.readLine());
 
-            System.out.println(pass.toString());
+            System.err.println(pass.toString());
             if (pass.isEquals()){
 
                 break;
@@ -18,63 +18,5 @@ public class Main {
         }
 
     }
-    public static class Password{
-        private String password;
-        private String sample;
 
-        String getPassword() {
-            return password;                //Гетер пароля
-        }
-
-        void setPassword(String password) {          //Сетер пароля
-            this.password = password;
-
-        }
-
-        String getSample() {            //Гетер пароля-экземпляра
-            return sample;
-        }
-
-        void setSample(String sample) {    //Сетер пароля-экземпляра
-            this.sample = sample;
-        }
-
-        Password() {
-            this.password = "";
-        }
-        /*Сравнение введенного пароля и пароля-экземпляра*/
-        boolean isEquals(){
-            return getPassword().equals(getSample());
-        }
-        /*Процедура установки нового пароля(при желании)
-        Если N - пароль устанавливается автоматически*/
-        void setSamp(BufferedReader in) throws IOException, AWTException {
-            System.out.println("Задать новый пароль?(Y/N)");
-            if (in.readLine().equals("Y") ){
-                System.out.println("Задайте пароль:");
-                setSample(in.readLine());
-                //click(65,750);
-                /*эксперементальная функция очистки консоли
-                после установки нового пароля */
-            } else {
-                System.out.println("Пароль будет задан по умолчанию");
-                setSample("ABC123");
-            }
-        }
-        /*Процедура очистки консоли путем нажатия на кнопку
-        очистки указанием координат на экране*/
-        public static void click(int x, int y) throws AWTException{
-            Robot bot = new Robot();
-            bot.mouseMove(x, y);
-            bot.mousePress(InputEvent.BUTTON1_MASK);
-            bot.mouseRelease(InputEvent.BUTTON1_MASK);
-        }
-
-        @Override
-        public String toString() {
-            if (isEquals()) return "Верный пароль";
-            else return "Пароль введен неверно. Попробуйте еще раз.";
-
-        }
-    }
 }
---
+++ src/Password.java
@@ -0,0 +1,64 @@
+import java.awt.*;
+import java.awt.event.InputEvent;
+import java.io.BufferedReader;
+import java.io.IOException;
+
+public class Password{
+    private String password;
+    private String sample;
+
+    String getPassword() {
+        return password;                //Гетер пароля
+    }
+
+    void setPassword(String password) {          //Сетер пароля
+        this.password = password;
+
+    }
+
+    String getSample() {            //Гетер пароля-экземпляра
+        return sample;
+    }
+
+    void setSample(String sample) {    //Сетер пароля-экземпляра
+        this.sample = sample;
+    }
+
+    Password() {
+        this.password = "";
+    }
+    /*Сравнение введенного пароля и пароля-экземпляра*/
+    boolean isEquals(){
+        return getPassword().equals(getSample());
+    }
+    /*Процедура установки нового пароля(при желании)
+    Если N - пароль устанавливается автоматически*/
+    void setSamp(BufferedReader in) throws IOException, AWTException {
+        System.out.println("Задать новый пароль?(Y/N)");
+        if (in.readLine().equals("Y") ){
+            System.out.println("Задайте пароль:");
+            setSample(in.readLine());
+            //click(65,750);
+                /*эксперементальная функция очистки консоли
+                после установки нового пароля */
+        } else {
+            System.out.println("Пароль будет задан по умолчанию");
+            setSample("ABC123");
+        }
+    }
+    /*Процедура очистки консоли путем нажатия на кнопку
+    очистки указанием координат на экране*/
+    public static void click(int x, int y) throws AWTException{
+        Robot bot = new Robot();
+        bot.mouseMove(x, y);
+        bot.mousePress(InputEvent.BUTTON1_MASK);
+        bot.mouseRelease(InputEvent.BUTTON1_MASK);
+    }
+
+    @Override
+    public String toString() {
+        if (isEquals()) return "Верный пароль";
+        else return "Пароль введен неверно. Попробуйте еще раз.";
+
+    }
+}
